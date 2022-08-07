a4043ea0786f5cf0583fb9fd634a19a33c0e2549
unknown <justinchervony@gmail.com>
2022-08-07 17:37:13 -0400

    General UML layout placed

---
+++ dinosaur.py
@@ -0,0 +1,6 @@
+class Dinosaur:
+    def __init__(self, name, attack_power):
+        pass
+    def attack(self, robot):
+        pass
+    
---
+++ robot.py
@@ -0,0 +1,7 @@
+from weapon import Weapon
+
+class Robot:
+    def __init__(self, name):
+        pass
+    def attack(self, dinosaur):
+        pass
