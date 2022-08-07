1afcb2d82dd3c6db1ad95f4fbb1edf77ef6903e4
unknown <justinchervony@gmail.com>
2022-08-07 19:56:21 -0400

    Added randomization to attack outcomes.

--- dinosaur.py
+++ dinosaur.py
@@ -1,8 +1,21 @@
+
+
+
+import random
+
+
 class Dinosaur:
     def __init__(self, name, attack_power):
         self.name = name
         self.attack_power = attack_power
         self.health = 100
     def attack(self, robot):
-        robot.health -= self.attack_power
-        print(f"{self.name} attacks! {robot.name} takes {self.attack_power} damage!")
+        attack_result = random.randint(1,100)
+        if attack_result <= 5:
+            print(f"{self.name} attacks but misses {robot.name}!")
+        elif attack_result >= 96:
+            robot.health -= self.attack_power*1.5
+            print(f"{self.name} attacks and scores a CRITICAL HIT! {robot.name} takes {self.attack_power*1.5} damage!")
+        else:
+            robot.health -= self.attack_power
+            print(f"{self.name} attacks! {robot.name} takes {self.attack_power} damage!")
--- robot.py
+++ robot.py
@@ -1,3 +1,5 @@
+
+import random
 from weapon import Weapon
 
 class Robot:
@@ -8,5 +10,12 @@ class Robot:
         self.active_weapon = Weapon("Laser Sword", 30)
         pass
     def attack(self, dinosaur):
-        dinosaur.health -= self.active_weapon.attack_power
-        print(f"{self.name} attacks! {dinosaur.name} takes {self.active_weapon.attack_power} damage!")
+        attack_result = random.randint(1,100)
+        if attack_result <= 5:
+            print(f"{self.name} attacks but misses {dinosaur.name}!")
+        elif attack_result >= 96:
+            dinosaur.health -= self.active_weapon.attack_power*1.5
+            print(f"{self.name} attacks and scores a CRITICAL HIT! {dinosaur.name} takes {self.active_weapon.attack_power*1.5} damage!")
+        else:
+            dinosaur.health -= self.active_weapon.attack_power
+            print(f"{self.name} attacks! {dinosaur.name} takes {self.active_weapon.attack_power} damage!")
