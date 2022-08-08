46fb652955cfa8b9b963ace84d67817be5491680
unknown <justinchervony@gmail.com>
2022-08-08 01:07:35 -0400

    Adjusted comments. Solidified dino ability aesthetic.

--- dinosaur.py
+++ dinosaur.py
@@ -8,18 +8,31 @@ class Dinosaur:
     def __init__(self, name, attack_power):
         self.name = name
         self.attack_power = attack_power
-        self.health = 100
+        self.health = 200
+        self.crit_buff = 0
+        self.turns_since_crit = 0
+
+
     def attack(self, robot):
         attack_result = random.randint(1,100)
-        if robot.active_weapon.name == "Gun":
-            attack_result -= 20
-        elif robot.active_weapon.name == "Kick":
-            attack_result -= 10
+        attack_result += self.crit_buff
+        if self.turns_since_crit < 3: #prevents spamming of abilities to negate crits for the whole fight
+            if robot.active_weapon.name == "Gun":
+                attack_result -= 20
+            elif robot.active_weapon.name == "Kick":
+                attack_result -= 10
+        else:
+            pass
+
         if attack_result <= 5:
             print(f"{self.name} attacks but misses {robot.name}!")
-        elif attack_result >= 96:
+            self.turns_since_crit += 1
+        elif attack_result >= 81: #dino has higher crit chance
             robot.health -= self.attack_power*1.5
+            self.crit_buff += 20 #if dino crits, they boost their crit chance significantly
             print(f"{self.name} attacks and scores a CRITICAL HIT! {robot.name} takes {self.attack_power*1.5} damage!")
+            self.turns_since_crit = 0
         else:
             robot.health -= self.attack_power
             print(f"{self.name} attacks! {robot.name} takes {self.attack_power} damage!")
+            self.turns_since_crit += 1
--- robot.py
+++ robot.py
@@ -6,7 +6,7 @@ class Robot:
     def __init__(self, name):
 
         self.name = name
-        self.health = 100
+        self.health = 200
         self.active_weapon = Weapon("Sword", 30)
         self.kick_debuff = 0
 
@@ -14,11 +14,11 @@ class Robot:
     def attack(self, dinosaur):
         weapon_choice = input(f"Choose {self.name}'s weapon. Type 'Sword', 'Gun', or 'Kick'. ")
         if weapon_choice == "Sword":
-            self.active_weapon = Weapon("Sword", 30)
+            self.active_weapon = Weapon("Sword", 30) #Standard weapon with same damage as dino
         elif weapon_choice == "Gun":
-            self.active_weapon = Weapon("Gun", 25)
+            self.active_weapon = Weapon("Gun", 25) #Weaker weapon, but reduces "accuracy" of dino by 20%
         elif weapon_choice == "Kick":
-            self.active_weapon = Weapon("Kick", 15)
+            self.active_weapon = Weapon("Kick", 10) #Weakest weapon, but adds damage each round of use and reduces dino's accuracy by 10%
         else:
             pass
 
